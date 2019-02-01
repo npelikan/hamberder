61d99cd7ddb1fca91a3cfd433a391c79a2727fde
nick.pelikan <nick.pelikan@yougov.com>
2019-02-01 11:13:23 -0700

    replacement rule testing

--- tests/test_replacement_rules.py
+++ tests/test_replacement_rules.py
@@ -1,7 +1,19 @@
 import pytest
-from hamberder.replacement_rules import define_replaceable
+from hamberder.replacement_rules import define_replaceable, replacement_rules
+import spacy
 
 
 def test_raises_error():
     with pytest.raises(TypeError):
         define_replaceable("hi, mom!")
+
+
+def test_replaces_stupid_catchphrase():
+    nlp = spacy.load('en_core_web_sm')
+    no_ampersand = nlp("Build the wall and Crime will fall!")
+    sent = list(no_ampersand.sents)[0]
+    assert "MORE BERDERS, LESS MURDERS!" == replacement_rules(sent)
+
+    yes_ampersand = nlp("Build the wall & Crime will fall!")
+    sent = list(yes_ampersand.sents)[0]
+    assert "MORE BERDERS, LESS MURDERS!" == replacement_rules(sent)
